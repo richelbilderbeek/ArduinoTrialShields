65f30f51c202c00073f64005ef2970e83b12a0c9
richelbilderbeek <richel@richelbilderbeek.nl>
2015-04-02 22:24:31 +0200

    Added blue belt

--- LedTrialShieldBlueBelt/blueBeltSolution/blueBeltSolution.ino
+++ LedTrialShieldBlueBelt/blueBeltSolution/blueBeltSolution.ino
@@ -1,15 +1,18 @@
 const int ledPin = 9;    
 
-void setup() {
+void setup() 
+{
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  for (int fadeValue = 0 ; fadeValue <= 255; ++fadeValue) {
+  for (int fadeValue = 0 ; fadeValue < 256; ++fadeValue) 
+  {
     analogWrite(ledPin, fadeValue);
     delay(10);
   }
-  for (int fadeValue = 0 ; fadeValue <= 255; --fadeValue) {
+  for (int fadeValue = 255 ; fadeValue > -1; --fadeValue) 
+  {
     analogWrite(ledPin, fadeValue);
     delay(10);
   }
