d39368fb4774b35f3d4d94a18e7bbef541ccbef7
thijsvb <thijsvb@gmail.com>
2015-04-02 22:23:53 +0200

    Added blue belt observer

--- LedTrialShieldBlueBelt/BlueBeltObserver/BlueBeltObserver.ino
+++ LedTrialShieldBlueBelt/BlueBeltObserver/BlueBeltObserver.ino
@@ -1,13 +1,48 @@
+const int led = A0;
+const int successPin = 12;
+
+int pointA;
+int pointB;
+int pointC;
+int check = 0;
+
 void setup() {
   Serial.begin(9600);
-  pinMode(11,OUTPUT);
+  pinMode(led,INPUT);
+  pinMode(successPin,OUTPUT);
+  Serial.println("Start Trial");
 }
 
 // the loop routine runs over and over again forever:
 void loop() 
 {
-  const int sensorValue = analogRead(A0);
-  analogWrite(11,map(sensorValue,0,1024,0,255));
-  Serial.println(sensorValue);
-  delay(100);
+  const int sensorValue = analogRead(led);
+  if(sensorValue == 0){
+    delay(1000);
+    pointA = analogRead(led);
+    if(pointA > 0){
+      delay(1550);
+      pointB = analogRead(led);
+      if(pointB > pointA){
+        delay(1550);
+        pointC = analogRead(led);
+        if(pointC > 0 && pointC < pointB){
+          ++check;
+        }
+        else{
+          check = 0;
+        }
+      }
+      else{
+        check = 0;
+      }
+    }
+    else{
+      check = 0;
+    }
+  }
+  
+  if(check == 2){
+    tone(successPin,440,1000);
+  }
 }
